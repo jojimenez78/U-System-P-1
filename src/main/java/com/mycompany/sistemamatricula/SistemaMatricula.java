18002b2f45d045760b706a96b98f3dcd2bc71667
jojimenez78 <garciaalonso527@gmail.com>
2024-10-17 11:44:58 -0600

    p

---
+++ src/main/java/com/mycompany/sistemamatricula/SistemaMatricula.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemamatricula;
+
+/**
+ *
+ * @author Usuario
+ */
+public class SistemaMatricula {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
